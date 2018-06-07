6a27f781842b84320ac1492426906ce49cc03df4
n8jhj <n8jhj@live.com>
2018-06-06 21:32:57 -0700

    editing score_keeper project

--- 4_dom_manipulation/score_keeper/script.js
+++ 4_dom_manipulation/score_keeper/script.js
@@ -6,17 +6,15 @@ var p2Display = document.getElementById('p2Score');
 var goalInput = document.getElementById('goalInput');
 var goalNum = document.getElementById('goalNum');
 
-var goal = 5;
 var p1Score = 0;
 var p2Score = 0;
-
+var goal = 5;
 var gameOver = false;
 
 p1Button.addEventListener('click', function(){
   if(!gameOver) {
     p1Score++;
     p1Display.textContent = p1Score;
-    console.log(p1Score, goal);
     if(p1Score === goal) {
       p1Display.style.color = 'green';
       gameOver = true;
@@ -35,17 +33,20 @@ p2Button.addEventListener('click', function(){
   }
 });
 
-resetButton.addEventListener('click', function(){
-  p1Score = 0;
-  p2Score = 0;
-  gameOver = false;
-  p1Display.textContent = p1Score;
-  p2Display.textContent = p2Score;
-  p1Display.style.color = 'black';
-  p2Display.style.color = 'black';
-});
+resetButton.addEventListener('click', resetGame);
 
-goalInput.addEventListener('click', function(){
+goalInput.addEventListener('change', function(){
   goal = this.valueAsNumber;
   goalNum.textContent = this.value;
+  resetGame();
 });
+
+function resetGame() {
+  p1Score = 0;
+  p1Display.textContent = p1Score;
+  p1Display.style.color = 'black';
+  p2Score = 0;
+  p2Display.textContent = p2Score;
+  p2Display.style.color = 'black';
+  gameOver = false;
+}
